7722a50a3984b0f791e9b3468b19edbea8504656
LayersOfAbstraction <jnash51@live.com>
2019-12-20 09:04:06 +1000

    Add DB seed initializer and DB context.

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        //NOTE dbSet creates dbSet property for each entity set. We don't need to specify the other database entities as EFC includes 
+        //them. We just do Students because it references the Enrollment table which also references the Course table.
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
