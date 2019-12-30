dbd0ba6b0d1be296e86e030f5bb78d1057595a0f
LayersOfAbstraction <jnash51@live.com>
2019-12-30 14:15:35 +1000

    Update of validation and display for FirstMidName and LastName property

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string LastName { get; set; }
+        [StringLength(50)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
         //Specifies a data type that's more specific than the database intrinsic type.
         //It can enable type specific features, for example a datetime picker
         //or for EmailAddress we can use a mailto: link.
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; } 
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         //This is a navigation property. It holds other entities that are related to the entity that
         //owns this property. This property holds all of the Enrollment entities related to the Student. 
