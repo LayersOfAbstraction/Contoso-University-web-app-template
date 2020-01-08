230b20c8a6f5381afffa8cad748a9e643d075f55
LayersOfAbstraction <jnash51@live.com>
2020-01-08 22:53:32 +1000

    Add file

---
+++ ContosoUniversity/Controllers/InstructorsController.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public InstructorsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// // GET: Instructors and do Eager loading of related data.
+        /// //Creates instance of the view model and puts in the list of instructors.
+        /// </summary>
+        /// <param name="id">Optional route data for InstructorID</param>
+        /// <param name="courseID">Optional route data for CourseID</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Index(int? id, int? courseID)
+        {
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = await _context.Instructors
+                //Eager loading used on this navigational property.                
+                .Include(i => i.OfficeAssignment)
+                //Eager loading used on this navigational property. Because the view requires this it's more
+                //efficient to fetch that in the same query.
+                .Include(i => i.CourseAssignments)
+                    //Inside CourseAssignments nav prop, these properties are loaded below.
+                    .ThenInclude(i => i.Course)
+                        //You must call the model for each of it's properties you select.
+                        .ThenInclude(i => i.Enrollments)
+                            //You don't need this.
+                            .ThenInclude(i => i.Student)
+                .Include(i => i.CourseAssignments)
+                 //Executes when an instructor was selected in the view model list.  
+                 .ThenInclude(i => i.Course)
+                    .ThenInclude(i => i.Department)
+                .AsNoTracking()
+                .OrderBy(i => i.LastName)
+                .ToListAsync();
+            
+            //If instructor selected with everything also related to it's navigational property.
+            if(id != null)
+            {
+
+                ViewData["InstructorID"] = id.Value;
+                //SELECT Instructor.
+                Instructor instructor = viewModel.Instructors.Where(
+                    i => i.ID == id.Value).Single();                
+                viewModel.Courses = instructor.CourseAssignments.Select(s => s.Course);
+            }
+
+            if(id != null)
+            {
+                ViewData["CourseID"] = courseID.Value;
+                viewModel.Enrollments = viewModel.Courses.Where(
+                    x => x.CourseID == courseID).Single().Enrollments;
+            }
+            return View(viewModel);
+        }
+
+        // GET: Instructors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+        }
+
+        // GET: Instructors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Instructors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(instructor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instructor);
+        }
+
+        // GET: Instructors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: Instructors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (id != instructor.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instructor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstructorExists(instructor.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instructor);
+        }
+
+        // GET: Instructors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+        }
+
+        // POST: Instructors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var instructor = await _context.Instructors.FindAsync(id);
+            _context.Instructors.Remove(instructor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InstructorExists(int id)
+        {
+            return _context.Instructors.Any(e => e.ID == id);
+        }
+    }
+}
