02da23566e402677eb3ac722ad079a35556c1f72
LayersOfAbstraction <jnash51@live.com>
2020-01-22 15:19:11 +1000

    Add tracking column rowversion byte array

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -22,5 +22,17 @@ namespace ContosoUniversity.Models
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
+
+        public int? InstructorID { get; set; }
+
+        //Attribute specifies this column will be included in the Where clasue
+        //of Update and Delete commands sent to DB.
+        //SQL version of this is "rowversion".
+        //The .NET type for "rowversion" is byte array.
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        public Instructor Administrator { get; set; }
+        public ICollection<Course> Courses { get; set; }
     }
 }
