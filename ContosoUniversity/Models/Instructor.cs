216b2cbbf6d4953ed376131a77d1c7b9d2b9c468
LayersOfAbstraction <jnash51@live.com>
2020-01-27 10:34:25 +1000

    Update Instructor

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -7,36 +7,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Instructor
+    public class Instructor : Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(50)]
-        public string LastName { get; set; }
-
-        [Required]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        [StringLength(50)]
-        public string FirstMidName { get; set; }
-
         //NOTE: We could have done all these attributes on seprate lines. This is another way of doing it.
         [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime HireDate { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get { return LastName + ", " + FirstMidName; }
-        }
-
+        
         //Navigation properties.
         //One instructor can have many assignments so this property will be a collection.
         public ICollection<CourseAssignment> CourseAssignments { get; set; }
         //One instructor can have one office so this does not need to be a collection.
-        public OfficeAssignment OfficeAssignment { get; set; }
-        
+        public OfficeAssignment OfficeAssignment { get; set; }        
     }
 }
