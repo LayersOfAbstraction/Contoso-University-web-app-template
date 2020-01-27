d0ab53008fe76235410d906f3768b096e09ac918
LayersOfAbstraction <jnash51@live.com>
2020-01-27 10:33:42 +1000

    Update Student

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -7,32 +7,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }        
-        [Display(Name = "Last Name")]
-        [Required]
-        [StringLength(50, MinimumLength = 2)]
-        public string LastName { get; set; }
-        [Required]
-        [StringLength(50)]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
-        //Specifies a data type that's more specific than the database intrinsic type.
-        //It can enable type specific features, for example a datetime picker
-        //or for EmailAddress we can use a mailto: link.
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; } 
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
         //This is a navigation property. It holds other entities that are related to the entity that
         //owns this property. This property holds all of the Enrollment entities related to the Student. 
